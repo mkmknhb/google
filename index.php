f298e19dafd3f2196e09a9bb24ebf872137fcd14
mjix <admin@minda.com.cn>
2014-06-15 15:34:30 +0800

    update readme

--- index.php
+++ index.php
@@ -5,15 +5,14 @@
  *      Author: vartu
  *      LastModifed:2014-06-15
  *      copyright(c)2004 idjay@126.com
- *      http://www.axx.com.cn
+ *      http://www.mjix.com
  ****************************************************/
 
 /*
 //if u web host bind mutiple domainname
-if($_SERVER['HTTP_HOST']=='axx.com.cn'||$_SERVER['HTTP_HOST']=='www.axx.com.cn'){
+if($_SERVER['HTTP_HOST']=='cctv.com'||$_SERVER['HTTP_HOST']=='cctv.com'){
     include 'search/home.html';
     exit();
 }*/
 
-//example site: http://axx.com.cn
 include 'search/home.html';
